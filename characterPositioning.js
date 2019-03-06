e6a1f4a54fbd06880353c27ff8f5179c30e3b112
Michael Cain <michaelcaincb@gmail.com>
2019-03-06 18:49:37 +0000

    charPositioning pushed index

---
+++ characterPositioning.js
@@ -0,0 +1,23 @@
+function countcharPositioning(phrase) {
+    var newPhrase = phrase.split(' ').join('')
+   
+    var output = {}
+
+    // arr.indexOf(searchElement[, fromIndex])
+    // console.log(beasts.indexOf('bison'));
+   
+   for (i = 0; i < newPhrase.length; i++) {
+    if (!output[newPhrase[i]]) {
+      output[newPhrase[i]] = [i]
+    } else {
+      output[newPhrase[i]].push([i]);
+    }
+   }
+   return output
+   }
+
+
+   
+   
+   
+   console.log(countcharPositioning('lighthouse in the house'));
